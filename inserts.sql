0ed32de65f78f616e7cff643ec42fdce2b3aa842
Eric Petit <9199.joan23@fje.edu>
2016-10-18 15:17:41 +0200

    V1.4
    He añadido el sql

---
+++ inserts.sql
@@ -0,0 +1,180 @@
+-- Insertamos datos de la tabla cajas
+
+
+INSERT INTO tbl_cajas
+                      (nombre_caja, tipo_caja)
+VALUES     ("Agridulce", "mediana")
+
+INSERT INTO tbl_cajas
+                      (nombre_caja, tipo_caja)
+VALUES     ("Avellanas celestiales", "pequeña")
+
+INSERT INTO tbl_cajas
+                      (nombre_caja, tipo_caja)
+VALUES     ("Coleccion Viento del Norte", "grande");
+
+--Insertamos datos de la tabla cesta
+
+INSERT INTO tbl_cesta
+                      (nombre_cesta, tipo_cesta)
+VALUES     ("Muestrario de verano", "Regalo cliente preferente");
+
+INSERT INTO tbl_cesta
+                      (nombre_cesta, tipo_cesta)
+VALUES     ("Surtido de otoño", "Regalo cliente preferente");
+
+INSERT INTO tbl_cesta
+                      (nombre_cesta, tipo_cesta)
+VALUES     ("Primavera en el aire", "Regalo cliente preferente");
+
+-- Insertamos En la tabla bombones
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B01", "Extasis Crepuscular", "Crema de Moca", "Anacardo", 0.33 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B02", "Mora Agridulce", "Mora", "Ninguno", 0.26 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B04", "Frambuesa Agridulce", "Fresa", "Ninguno", 0.24 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B05", "Baya Agridulce", "Baya", "Ninguno", 0.26 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B06", "Mermelada Agridulce", "Mermelada", "Ninguno", 0.17 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B07", "Cereza Agridulce", "Cereza entera", "Ninguno", 0.27 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("B08", "Avellana Amarga", "Avellana", "Ninguno", 0.24 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D01", "Almendra Suprema", "Ninguno", "Almendra", 0.35 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D02", "Corazón Envuelto", "Crema de Cereza", "Ninguno", 0.35 );
+
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D03", "Anacardo Suprema", "Ninguno", "Anacardo", 0.37 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D04", "Almendra Ambrosia", "Amaretto", "Almendra", 0.49 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D06", "Avellana Suprema", "Ninguno", "Avellana", 0.35 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D07", "Cereza Clásica", "Ninguno", "Cereza entera", 0.35 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D09", "Belleza Americana", "Ninguno", "Ninguno entera", 0.35 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("D11", "Mazapán Delicia", "Ninguno", "Mazapán", 0.35 );
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("F01", "Nuez Moca", "Nuez", "Crema de Moca", 0.35 );
+
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("F02", "Pistacho Moca","Crema de Moca","Pistacho",0.21);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M01", "Frambuesa dulce","Fresa","Ninguna", 0.20);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M02", "Macadamia Suprema","Ninguno","Macadamia", 0.40);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M03", "Pistacho Supremo","Ninguno","Pistacho",0.30);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M04", "Delicia Mantequilla","CacahueteMantequilla de cacahuete","Ninguna",0.21);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M05", "Mazapán Pinzón","Mazapán","Ninguna",0.32);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M06", "Corazón Amante","Crema de Cereza","Ninguna",0.25);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M08", "Arce de Mazapán","Mazapán","Ninguna",0.37);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M09", "Baya Dulce","Baya","Ninguna",0.26);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M10", "No me olvides","Mora","Ninguna",0.19);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M11", "Cereza Dulce","Cereza entera","Ninguna",0.27);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M12", "Baya Dulce","Mora","Ninguna",0.25);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M13", "Mermelada Dulce","Mermelada","Ninguna",0.18);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M14", "Avellana Virgen","Crema de cereza","Avellana",0.26);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M15", "Palma Tropical","Coco","Ninguna",0.23);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M16", "Maravilla Mazapán","Mazapán","Ninguna",0.33);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M17", "Avellana Amaretto","Amaretto","Avellana",0.36);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("M18", "Avellana Moca","Crema de Moca","Avellana",0.33);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("W01", "Golondrina de Mazapán","Mazapán","Ninguna",0.34);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("W02", "Calla Lily","Ninguno","Ninguna",0.27);
+
+INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+VALUES
+("W03", "Corazón Destrozado","Ninguno","Avellana",0.30);
