c22a4511d77bc339c0664331793c6c1e9ef7f2b7
ivan-lh96 <10000713.joan23@fje.edu>
2016-10-18 19:30:23 +0200

    Insercion de datos
    Hemos insertado casi todos los datos de la tabla de anuncios en SQL

--- inserts.sql
+++ inserts.sql
@@ -1,180 +1,91 @@
--- Insertamos datos de la tabla cajas
 
-
-INSERT INTO tbl_cajas
-                      (nombre_caja, tipo_caja)
-VALUES     ("Agridulce", "mediana")
-
-INSERT INTO tbl_cajas
-                      (nombre_caja, tipo_caja)
-VALUES     ("Avellanas celestiales", "pequeña")
-
-INSERT INTO tbl_cajas
-                      (nombre_caja, tipo_caja)
-VALUES     ("Coleccion Viento del Norte", "grande");
-
---Insertamos datos de la tabla cesta
-
-INSERT INTO tbl_cesta
-                      (nombre_cesta, tipo_cesta)
-VALUES     ("Muestrario de verano", "Regalo cliente preferente");
-
-INSERT INTO tbl_cesta
-                      (nombre_cesta, tipo_cesta)
-VALUES     ("Surtido de otoño", "Regalo cliente preferente");
-
-INSERT INTO tbl_cesta
-                      (nombre_cesta, tipo_cesta)
-VALUES     ("Primavera en el aire", "Regalo cliente preferente");
-
--- Insertamos En la tabla bombones
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B01", "Extasis Crepuscular", "Crema de Moca", "Anacardo", 0.33 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Me han robado una Bici de carretera", "2016-01-15", "2016-01-14", "Barcelona", "Me la robaron un grupo de chavales al poco de dejarla en el aparcamiento de bicis", "BH", "Spehene claris 2015", "Rojo", "Nueva", "", "123412341234123", "0001.jpg", 20.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B02", "Mora Agridulce", "Mora", "Ninguno", 0.26 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Bici de carretera perdida", "2016-05-20", "2016-05-19", "Lleida", "Fue robada durante la noche en la calle mientras estaba en casa", "BH", "Quartz 105", "Gris", "Nueva", "", "123412341234124", "0002.jpg", 35.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B04", "Frambuesa Agridulce", "Fresa", "Ninguno", 0.24 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Mi bici urbana ha desaparecido", "2015-10-25", "2015-10-23", "Tarragona", "Alguien me robó la bici y desde hace dos dias no se sabe nada de ella", "Merida", "Speeder 400 2015", "Verde", "Menos de 5 años", "", "123412341234125", "0003.jpg", 25.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B05", "Baya Agridulce", "Baya", "Ninguno", 0.26 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("¡Urgente, bici robada!", "2016-10-15", "2016-10-14", "Girona", "En la noche de ayer un chico alto con la cara tapada me robó la bici y me agredió", "Merida", "Speeder 100", "Negro", "Menos de 5 años", "", "123412341234126", "0004.jpg", 10.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B06", "Mermelada Agridulce", "Mermelada", "Ninguno", 0.17 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("BMX robada mientras estudiaba", "2016-07-05", "2016-07-04", "Tarragona", "Mi BMX me la robaron cuando estaba en el instituto y no vi quien fué", "Monty Bikes", "BMX 139", "Amarillo", "Mas de 5 años", "", "123412341234127", "0005.jpg", 5.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B07", "Cereza Agridulce", "Cereza entera", "Ninguno", 0.27 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Sigo sin encontrar mi bici desaparecida" "2015-03-07", "2015-03-06", "Barcelona", "Hace mas de año y medio que perdí la bici y sigo sin encontrarla, me la robaron cerca de Diagonal", "Monty Bikes", "Free 301", "Negro", "Mas de 5 años", "", "123412341234128", "0006.jpg", 8.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("B08", "Avellana Amarga", "Avellana", "Ninguno", 0.24 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Necesito recuperar mi Crossway azul", "2016-09-20", "2016-09-17", "LLeida", "Me robaron la bici un viernes por la noche y llevo todo el fin de semana buscándola sin fortuna", "Merida", "Crossway 40", "Azul", "Nueva", "", "123412341234129", "0007.jpg", 23.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D01", "Almendra Suprema", "Ninguno", "Almendra", 0.35 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("BH Renegade robada", "2016-10-12", "2016-10-08", "Girona", "Mi BH Renegade fue robada la semana pasada por un grupo de gitanos al lado de la estación y ya no la he vuelto a ver", "BH", "Renegade", "Negra", "Nose como está", "", "123412341234130", "0008.jpg", 12.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D02", "Corazón Envuelto", "Crema de Cereza", "Ninguno", 0.35 );
-
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Sigo sin encontrar mi Ciclocross nueva,es urgente", "2016-08-19", "2016-08-18", "Barcelona", "Hace dos meses que perdí mi nueva Ciclocross y necesito encontrarla porque me costó mucho dinero", "Merida", "Ciclo Cross 6000", "Azul", "Nueva", "", "123412341234131", "0009.jpg", 100.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D03", "Anacardo Suprema", "Ninguno", "Anacardo", 0.37 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Bicicleta robada en Girona", "2015-06-15", "2015-06-14", "Girona", "El año pasado en la plaza del centro de Girona me robaron la bici forzando el candado donde estaba atada", "Merida", "Ciclo cross 500", "Negro", "Menos de 5 años", "", "123412341234132", "0010.jpg", 70.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D04", "Almendra Ambrosia", "Amaretto", "Almendra", 0.49 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Fat Bike desaparecida por la zona de Sants", "2016-04-24", "2016-01-22", "Barcelona", "El dia de Sant Jordi me la robó un chico en Sants mientras estaba comprando una rosa", "Felt", "Felt DD70", "Negro", "Menos de 5 años", "", "123412341234133", "0011.jpg", 80.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D06", "Avellana Suprema", "Ninguno", "Avellana", 0.35 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Bici robada en navidad", "2015-12-26", "2015-12-25", "Lleida", "Las navidades pasadas mi bici fue robada justo delante de mi casa por unos chavales menores y aun no la he podido encontrar", "Felt", "Felt DD30", "Azul", "Nueva", "", "123412341234134", "0012.jpg", 35.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D07", "Cereza Clásica", "Ninguno", "Cereza entera", 0.35 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Btwin robada en un poligono", "2016-11-03", "2016-11-07", "Barcelona", "La bicicleta fue robada en el poligono industrial el pasado dia 3 por la noche", "Btwin", "Nework 700", "Negro", "Mas de 5 años", "", "123412341234135", "0013.jpg", 25.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D09", "Belleza Americana", "Ninguno", "Ninguno entera", 0.35 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Bicicleta electrica perdida en Hospitalet de Llobregat", "2016-07-28", "2016-07-27", "Barcelona", "El pasado 27 de julio me robaron mi devron electrica cuando estaba intentado sacarla del aparcabicis", "Devron", "Devron 28122", "Rojo", "Nueva", "", "123412341234136", "0014.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("D11", "Mazapán Delicia", "Ninguno", "Mazapán", 0.35 );
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Monty Bike electrica desaparecida en Girona", "2016-09-29", "2016-09-30", "Girona", "La bici me la robaron mientras estaba en el trabajo", "Monty Bikes", "E 49", "Blanco", "Nueva", "", "123412341234137", "0015.jpg", 40.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("F01", "Nuez Moca", "Nuez", "Crema de Moca", 0.35 );
-
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Necesito recuperar mi bici Merida", "2015-10-22", "2015-10-20", "Tarragona", "La bici fue robada en el trastero por el mediodia", "Merida", "Matts Tfs 100-D", "Blanco", "Mas de 5 anyos", "", "123412341234138", "0016.jpg", 10.00 );
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("F02", "Pistacho Moca","Crema de Moca","Pistacho",0.21);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("MTB robada en casa", "2016-06-10", "2016-06-12", "Tarragona", "La bici fue robada en el balcon de un primer piso por dos personas", "Giant", "Talon 1", "Negro", "Mas de 5 anyos", "", "123412341234139", "0017.jpg", 20.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M01", "Frambuesa dulce","Fresa","Ninguna", 0.20);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("Bici triatlon robada en LLeida", "2016-02-25", "2016-02-24", "Lleida", "Bici robada en el centro de Lleida cerca de un parque", "Felt", "B16", "Negro", "Menos de 5 anyos", "", "123412341234140", "0018.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M02", "Macadamia Suprema","Ninguno","Macadamia", 0.40);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2015-04-15", "2015-04-14", "Barcelona", "", "Felt", "B14", "Rojo", "Nueva", "", "123412341234141", "0019.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M03", "Pistacho Supremo","Ninguno","Pistacho",0.30);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234142", "0020.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M04", "Delicia Mantequilla","CacahueteMantequilla de cacahuete","Ninguna",0.21);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Lleida", "", "", "", "", "Nueva", "", "123412341234143", "0021.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M05", "Mazapán Pinzón","Mazapán","Ninguna",0.32);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234144", "0022.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M06", "Corazón Amante","Crema de Cereza","Ninguna",0.25);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234145", "0023.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M08", "Arce de Mazapán","Mazapán","Ninguna",0.37);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234146", "0024.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M09", "Baya Dulce","Baya","Ninguna",0.26);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234147", "0025.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M10", "No me olvides","Mora","Ninguna",0.19);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234148", "0026.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M11", "Cereza Dulce","Cereza entera","Ninguna",0.27);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Lleida", "", "", "", "", "Nueva", "", "123412341234149", "0027.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M12", "Baya Dulce","Mora","Ninguna",0.25);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234150", "0028.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M13", "Mermelada Dulce","Mermelada","Ninguna",0.18);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
+("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234151", "0029.jpg", 60.00);
+INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("M14", "Avellana Virgen","Crema de cereza","Avellana",0.26);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("M15", "Palma Tropical","Coco","Ninguna",0.23);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("M16", "Maravilla Mazapán","Mazapán","Ninguna",0.33);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("M17", "Avellana Amaretto","Amaretto","Avellana",0.36);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("M18", "Avellana Moca","Crema de Moca","Avellana",0.33);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("W01", "Golondrina de Mazapán","Mazapán","Ninguna",0.34);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("W02", "Calla Lily","Ninguno","Ninguna",0.27);
-
-INSERT INTO tbl_Bombon (id_Bombon, nombre_bombon, tipo_relleno, tipo_nuez, precio_bombon) 
-VALUES
-("W03", "Corazón Destrozado","Ninguno","Avellana",0.30);
+("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234152", "0030.jpg", 60.00);
