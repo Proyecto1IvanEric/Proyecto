8bed7eeb3ae2eeb0aacb12fcb3e9aa439461ec35
ivan-lh96 <10000713.joan23@fje.edu>
2016-10-19 18:18:48 +0200

    Inserts y values acabados
    Hemos acabado los inserts y values y estamos añadiendo las imagenes

--- inserts.sql
+++ inserts.sql
@@ -25,13 +25,13 @@ VALUES
 ("BH Renegade robada", "2016-10-12", "2016-10-08", "Girona", "Mi BH Renegade fue robada la semana pasada por un grupo de gitanos al lado de la estación y ya no la he vuelto a ver", "BH", "Renegade", "Negra", "Nose como está", "", "123412341234130", "0008.jpg", 12.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("Sigo sin encontrar mi Ciclocross nueva,es urgente", "2016-08-19", "2016-08-18", "Barcelona", "Hace dos meses que perdí mi nueva Ciclocross y necesito encontrarla porque me costó mucho dinero", "Merida", "Ciclo Cross 6000", "Azul", "Nueva", "", "123412341234131", "0009.jpg", 100.00);
+("Sigo sin encontrar mi Ciclocross nueva,es urgente", "2016-08-19", "2016-08-18", "Barcelona", "Hace dos meses que perdí mi nueva Ciclocross y necesito encontrarla porque me costó mucho dinero", "Merida", "Ciclo Cross 6000", "Azul", "Nueva", "", "123412341234131", "0009.jpg", 85.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
 ("Bicicleta robada en Girona", "2015-06-15", "2015-06-14", "Girona", "El año pasado en la plaza del centro de Girona me robaron la bici forzando el candado donde estaba atada", "Merida", "Ciclo cross 500", "Negro", "Menos de 5 años", "", "123412341234132", "0010.jpg", 70.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("Fat Bike desaparecida por la zona de Sants", "2016-04-24", "2016-01-22", "Barcelona", "El dia de Sant Jordi me la robó un chico en Sants mientras estaba comprando una rosa", "Felt", "Felt DD70", "Negro", "Menos de 5 años", "", "123412341234133", "0011.jpg", 80.00);
+("Fat Bike desaparecida por la zona de Sants", "2016-04-24", "2016-01-22", "Barcelona", "El dia de Sant Jordi me la robó un chico en Sants mientras estaba comprando una rosa", "Felt", "Felt DD70", "Negro", "Menos de 5 años", "", "123412341234133", "0011.jpg", 55.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
 ("Bici robada en navidad", "2015-12-26", "2015-12-25", "Lleida", "Las navidades pasadas mi bici fue robada justo delante de mi casa por unos chavales menores y aun no la he podido encontrar", "Felt", "Felt DD30", "Azul", "Nueva", "", "123412341234134", "0012.jpg", 35.00);
@@ -46,7 +46,7 @@ VALUES
 ("Monty Bike electrica desaparecida en Girona", "2016-09-29", "2016-09-30", "Girona", "La bici me la robaron mientras estaba en el trabajo", "Monty Bikes", "E 49", "Blanco", "Nueva", "", "123412341234137", "0015.jpg", 40.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("Necesito recuperar mi bici Merida", "2015-10-22", "2015-10-20", "Tarragona", "La bici fue robada en el trastero por el mediodia", "Merida", "Matts Tfs 100-D", "Blanco", "Mas de 5 anyos", "", "123412341234138", "0016.jpg", 10.00 );
+("Necesito recuperar mi bici Merida", "2015-10-22", "2015-10-20", "Tarragona", "La bici fue robada en el trastero por el mediodia", "Merida", "Matts Tfs 100-D", "Blanco", "Mas de 5 anyos", "", "123412341234138", "0016.jpg", 10.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
 ("MTB robada en casa", "2016-06-10", "2016-06-12", "Tarragona", "La bici fue robada en el balcon de un primer piso por dos personas", "Giant", "Talon 1", "Negro", "Mas de 5 anyos", "", "123412341234139", "0017.jpg", 20.00);
@@ -55,37 +55,37 @@ VALUES
 ("Bici triatlon robada en LLeida", "2016-02-25", "2016-02-24", "Lleida", "Bici robada en el centro de Lleida cerca de un parque", "Felt", "B16", "Negro", "Menos de 5 anyos", "", "123412341234140", "0018.jpg", 60.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2015-04-15", "2015-04-14", "Barcelona", "", "Felt", "B14", "Rojo", "Nueva", "", "123412341234141", "0019.jpg", 60.00);
+("Felt roja robada en paseo de Gracia", "2015-04-15", "2015-04-14", "Barcelona", "La bici ha sido robada por unos chavales en paseo de Gracia la tarde del dia 14", "Felt", "B14", "Rojo", "Nueva", "", "123412341234141", "0019.jpg", 60.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234142", "0020.jpg", 60.00);
+("Bici junior robada", "2016-03-02", "2016-03-01", "Girona", "El pasado dia 1 la bici fue robada en el parking de nuestra casa", "Monty Bikes", "103", "Rojo", "Nueva", "", "123412341234142", "0020.jpg", 30.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Lleida", "", "", "", "", "Nueva", "", "123412341234143", "0021.jpg", 60.00);
+("Monty Bike junior desaparecida", "2016-08-20", "2016-08-19", "Lleida", "La bici de mi hijo desaparecio el pasado dia 19 y aun no ha aparecido", "Monty Bikes", "202 Pushbike", "Otro", "Nueva", "", "123412341234143", "0021.jpg", 20.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234144", "0022.jpg", 60.00);
+("No encuentro mi bici plegable", "2016-10-06", "2016-10-05", "Barcelona", "Mi bici Dahon plegable desaparecio el otro dia y desconozco quien pudo robarla", "Dahon", "Jifo", "Otro", "Nueva", "", "123412341234144", "0022.jpg", 50.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234145", "0023.jpg", 60.00);
+("Mi monty bike plegable ha sido robada", "2016-05-12", "2016-05-11", "Tarragona", "El pasado dia 11 mi Monty Bike plegable fue robada en el paseo mientras estaba aparcandola", "Monty Bikes", "Pulse", "Azul", "Nueva", "", "123412341234145", "0023.jpg", 15.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234146", "0024.jpg", 60.00);
+("Bici BH London Wave robada en vacaciones", "2015-07-18", "2015-07-17", "Girona", "Mi bici BH London Wave fue robada en frente del hotel donde pasamos las vacaciones", "BH", "London Wave", "Otro", "Nueva", "", "123412341234146", "0024.jpg", 13.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234147", "0025.jpg", 60.00);
+("Bici de cuadro abierta robada", "2015-11-05", "2015-11-04", "Barcelona", "La bici fue robada cerca de Sagrada Familia", "Colouer", "Colouer Vintage", "Otro", "Nueva", "", "123412341234147", "0025.jpg", 5.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234148", "0026.jpg", 60.00);
+("MTB robada dentro de mi portal", "2016-03-21", "2016-03-20", "Tarragona", "Mi MTB fue robada dentro del recinto de la comunidad por la manyana", "MTB", "Agece Scape", "Negro", "Nueva", "", "123412341234148", "0026.jpg", 27.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Lleida", "", "", "", "", "Nueva", "", "123412341234149", "0027.jpg", 60.00);
+("¡No encuentro mi bici de cuadros, ayuda!", "2016-06-23", "2016-06-22", "Lleida", "Desde el pasado mes de junio mi bici de cuadros desaparecio en frente de un bar de Lleida", "Ritchey", "P-29", "Otro", "Nueva", "", "123412341234149", "0027.jpg", 22.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Girona", "", "", "", "", "Nueva", "", "123412341234150", "0028.jpg", 60.00);
+("Bici de mujer robada", "2016-04-26", "2016-04-25", "Girona", "La bici de mi mujer fue robada en el trastero de casa por la noche", "Merida", "Juliet 6 10 V", "Verde", "Nueva", "", "123412341234150", "0028.jpg", 33.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Barcelona", "", "", "", "", "Nueva", "", "123412341234151", "0029.jpg", 60.00);
+("La bici de mi chica ha desaparecido", "2016-09-09", "2016-09-08", "Barcelona", "La bici de carreras de mi chica desaparecio mientras estabamos paseando por la Barceloneta", "Felt", "ZW100", "Negro", "Nueva", "", "123412341234151", "0029.jpg", 17.00);
 INSERT INTO tbl_anunci (anu_id, anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
 VALUES
-("", "2016-01-15", "2016-01-14", "Tarragona", "", "", "", "", "Nueva", "", "123412341234152", "0030.jpg", 60.00);
+("Necesito recuperar mi Monty Bike", "2016-10-18", "2016-10-17", "Tarragona", "El otro dia un grupo de chavales latinos la bici y tenia mucho valor para mi,necesito recuperarla", "Monty Bikes", "M5", "Otro", "Nueva", "", "123412341234152", "0030.jpg", 40.00);
